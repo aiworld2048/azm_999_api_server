4fc47cfd5bc6be45801168ec2b760f6ab23cf79e
AMK1215 <aungmyokhaing1215@gmail.com>
2025-11-17 14:50:39 +0630

    api new repo created by amk

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            PermissionsTableSeeder::class,
+            SubAgentPermissionSeeder::class,
+            RolesTableSeeder::class,
+            PermissionRoleTableSeeder::class,
+            UsersTableSeeder::class,
+            RoleUserTableSeeder::class,
+            GameTypeTableSeeder::class,
+            ProductSeeder::class,
+            
+
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+require_once __DIR__.'/admin.php';
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Authentication routes
+Route::get('/login', [LoginController::class, 'showLogin'])
+    ->name('login')
+    ->middleware('guest');
+Route::post('/login', [LoginController::class, 'login'])
+    ->name('login.attempt')
+    ->middleware('guest');
+Route::post('logout', [LoginController::class, 'logout'])
+    ->name('logout')
+    ->middleware('auth');
+Route::post('update-password/{user}', [LoginController::class, 'updatePassword'])
+    ->name('updatePassword')
+    ->middleware('auth');
+
